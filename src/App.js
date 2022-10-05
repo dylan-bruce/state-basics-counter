daeb4df8214d3e730715fade40d8efd890b1537e
Dylan Bruce <dyanb1127@gmail.com>
2022-10-05 16:06:13 -0400

    Counter Basics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  function addCount() {
+    setCount(count + 1)
+  }
+
+  function count5up() {
+    setCount(count + 5);
+  }
+
+  function count10up() {
+    setCount(count + 10)
+  }
+
+  function subtractCount() {
+    setCount(count - 1)
+  }
+  
+  function count5down() {
+    setCount(count - 5);
+  }
+
+  function count10down() {
+    setCount(count - 10);
+  }
+
+  function resetCount() {
+    setCount(0)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter </h1>
+      <hr/>
+      <button onClick={addCount}>+</button>
+      <button onClick={count5up}>++</button>
+      <button onClick={count10up}>+++</button>
+      <h2>{count}</h2>
+      <button onClick={subtractCount}>-</button>
+      <button onClick={count5down}>--</button>
+      <button onClick={count10down}>---</button>
+      <hr/>
+      <button onClick={resetCount}>Reset</button>
+      <hr/>
     </div>
   );
 }
